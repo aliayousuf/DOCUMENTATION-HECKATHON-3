fd09c759c71e8d11eaadb4d0714e58577d85e925
aliayousuf <160315316+aliayousuf@users.noreply.github.com>
2025-01-22 08:20:12 +0500

    Add files via upload

---
+++ Day 2 Marketplace Technical Foundation [Qcommerce]/SanitySchema.js
@@ -0,0 +1,46 @@
+
+// export default {
+//     name: 'food',
+//     type: 'document',
+//     title: 'Food',
+//     fields: [
+//       {
+//         name: 'name',
+//         type: 'string',
+//         title: 'Food Name',
+//       },
+      
+//       {
+//         name: 'price',
+//         type: 'number',
+//         title: 'Current Price',
+//       },
+//       {
+//         name: 'originalPrice',
+//         type: 'number',
+//         title: 'Original Price',
+//         description: 'Price before discount (if any)',
+//       },
+      
+//       {
+//         name: 'image',
+//         type: 'image',
+//         title: 'Food Image',
+//         options: {
+//           hotspot: true,
+//         },
+//       },
+//       {
+//         name: 'description',
+//         type: 'text',
+//         title: 'Description',
+//         description: 'Short description of the food item',
+//       },
+//       {
+//         name: 'available',
+//         type: 'boolean',
+//         title: 'Available',
+//         description: 'Availability status of the food item',
+//       },
+//     ],
+//   };
